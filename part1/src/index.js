251f27f8dd92932fad642e617c5be338674a803c
wallflower <emmmbes@gmail.com>
2020-05-19 18:24:15 +0800

    data

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-const App = () => (
-  <div>
-    <p>Hello world!</p>
-  </div>
-)
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
 
-ReactDOM.render(<App />, document.getElementById('root'))
+  return (
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
