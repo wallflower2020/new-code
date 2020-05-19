43f1caba040b58e7222f8638c91e7ec473076cba
wallflower <emmmbes@gmail.com>
2020-05-19 18:43:31 +0800

    george

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Hello = () => {
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello {props.name}</p>
     </div>
   )
 }
@@ -13,9 +13,8 @@ const App = () => {
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello />
-      <Hello />
-      <Hello />
+      <Hello name="George" />
+      <Hello name="Daisy" />
     </div>
   )
 }
