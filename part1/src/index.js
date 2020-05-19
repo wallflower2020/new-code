baa75213c136afae3b151b2b65796a702ab8ebb8
wallflower <emmmbes@gmail.com>
2020-05-19 19:06:11 +0800

    div

--- part1/src/index.js
+++ part1/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 
 const Hello = (props) => {
   return (
-    <div>
+    <>
       <p>
         Hello {props.name},you are {props.age} years old
       </p>
-    </div>
+    </>
   );
 };
 
@@ -16,11 +16,11 @@ const App = () => {
   const age = 10
 
   return (
-    <div>
+    <>
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
-    </div>
+    </>
   )
 }
 
