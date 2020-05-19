732258402f1ae633e854657acaddcefd788a6066
wallflower <emmmbes@gmail.com>
2020-05-19 18:41:23 +0800

    greetings

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 10
-
+const Hello = () => {
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
     </div>
   )
 }
