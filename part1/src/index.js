8cb02319567d59140f37cfdb62a1f12e23b338cf
wallflower <emmmbes@gmail.com>
2020-05-21 21:01:08 +0800

    plus mac

--- part1/src/index.js
+++ part1/src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from "react-dom";
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
-
+  const [ counter1, setCounter1 ] = useState(0)
   return (
     <div>
       <div>{counter}</div>
       <button onClick={() => setCounter(counter + 1)}>
         plus
       </button>
+      <button onClick={() => setCounter(0)}> 
+        zero
+      </button>
+
+      <div>{counter1}</div>
+      <button onClick={() => setCounter1(counter1 + 1)}>
+        plus
+      </button>
+      <button onClick={() => setCounter1(0)}> 
+        zero
+      </button>
+      <div>{counter + counter1}</div>
     </div>
   )
 }
