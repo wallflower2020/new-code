8f8202751f95c4140c36d1e6b55519677ad2414e
wallflower <emmmbes@gmail.com>
2020-05-21 20:24:29 +0800

    buhuo

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React, { useState } from 'react'
 import ReactDOM from "react-dom";
 
-const App = (props) => {
-  const { counter } = props
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
+  console.log('rendering...', counter)
+  
   return (
     <div>{counter}</div>
   )
 }
 
-let counter = 1
-
-const refresh = () => {
-  ReactDOM.render(<App counter={counter} />, 
-  document.getElementById('root'))
-}
-
-refresh()
-counter += 1
-refresh()
-counter += 1
-refresh()
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
 
